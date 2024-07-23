a27ae89ec0b5e8615c9983c435fe150c927907f0
Sam-Cowman <sam.p.cowman@gmail.com>
2024-07-23 16:01:10 -0500

    edited home routes with login and signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,5 +45,65 @@ router.get('/profile', withAuth, async (req, res) => {
     }
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+  
+  router.get('/signup', (req, res) => {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('signup');
+  });
+  
+  router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        where: {
+          user_id: req.session.user_id,
+        },
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('dashboard', {
+        posts,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['username'],
+          },
+          {
+            model: Comment,
+            include: [User],
+          },
+        ],
+      });
+  
+      const post = postData.get({ plain: true });
+  
+      res.render('post', {
+        ...post,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
